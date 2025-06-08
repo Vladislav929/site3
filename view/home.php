015bc2219d6ec3f35112eddcf4ad1130f693b73e
Vladislav929 <159521490+Vladislav929@users.noreply.github.com>
2025-06-08 21:30:24 +0300

    Create home.php

---
+++ view/home.php
@@ -0,0 +1,7 @@
+
+<?php require_once ROOT . '/view/templates/header.php'; ?>
+
+<h2>Добро пожаловать в Мебельный салон!</h2>
+<p>Выберите раздел в меню для работы с системой.</p>
+
+<?php require_once ROOT . '/view/templates/footer.php'; ?>
